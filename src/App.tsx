fd9e9f56b189bc383adcc0d2c0bee92153659dc2
rahmadyan95 <117942032+rahmadyan95@users.noreply.github.com>
2024-10-05 23:01:05 +0700

    Hello

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './assets/logo.png';
+import menulogo from './assets/menu.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='min-h-screen bg-greybackground'>
+      <nav className='bg-greybackground p-4'>
+        <div className='container mx-auto flex items-center justify-between p-1'>
+          <div className='flex items-center'>
+            <img src={logo} alt="logo" className="h-7 mr-2" />
+            <h1 className="text-white text-[20px] mb-1 md:mb-0 font-fira text-left">Rahmadyan</h1>
+          </div>
+
+          <div className='md:static absolute bg-greybackground md:min-h-fit min-h-[60vh] left-0 top-[-100%] md:w-auto w-full flex items-center px-5 '>
+            <ul className='flex md:flex-row flex-col  md:items-center md:gap-[4vw] gap-8 font-fira text-[19px]'>
+              <li>
+                <a href="#" className="flex items-center">
+                  <span className="text-purple-600">#</span>
+                  <span className="text-white">home</span>
+                </a>
+              </li>
+
+              <li>
+                <a href="#" className="flex items-center">
+                  <span className="text-purple-600">#</span>
+                  <span className="text-white">works</span>
+                </a>
+              </li>
+              
+              <li>
+                <a href="#" className="flex items-center">
+                  <span className="text-purple-600">#</span>
+                  <span className="text-white">about-me</span>
+                </a>
+              </li>
+  
+              <li>
+                <a href="#" className="flex items-center">
+                  <span className="text-purple-600">#</span>
+                  <span className="text-white">contacts</span>
+                </a>
+              </li>
+            </ul>
+          
+          
+          </div>
+           <div className='flex items-end md:gap-6'>
+            <button className="flex items-center" >
+            <img src={menulogo} alt="logo" className='h-7 mr-2 cursor-pointer md:hidden' />
+            </button>
+        </div>
+         
+        </div>
+      </nav>
     </div>
   );
 }
