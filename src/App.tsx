bf167a055abe23bb237861bb500e6aa51136c54f
rahmadyan95 <117942032+rahmadyan95@users.noreply.github.com>
2024-10-07 16:35:21 +0700

    add: Menu dropdown ahhh

--- src/App.tsx
+++ src/App.tsx
@@ -2,56 +2,72 @@ import './App.css';
 import logo from './assets/logo.png';
 import menulogo from './assets/menu.png';
 
+function Menu(e: React.MouseEvent<HTMLImageElement>) {
+  const list = document.querySelector('ul');
+  const target = e.target as HTMLImageElement; // Cast to HTMLImageElement
+  const menuStatus = target.getAttribute('data-status');
+
+  if (menuStatus === 'menu') {
+    target.setAttribute('data-status', 'close');
+    list?.classList.add('top-[80px]', 'opacity-100');
+  } else {
+    target.setAttribute('data-status', 'menu');
+    list?.classList.remove('top-[80px]', 'opacity-100');
+  }
+}
+
 function App() {
   return (
-    <div className='min-h-screen bg-greybackground'>
-      <nav className='bg-greybackground p-4'>
-        <div className='container mx-auto flex items-center justify-between p-1'>
-          <div className='flex items-center'>
-            <img src={logo} alt="logo" className="h-7 mr-2" />
-            <h1 className="text-white text-[20px] mb-1 md:mb-0 font-fira text-left">Rahmadyan</h1>
-          </div>
+    <div className='bg-greybackground'>
+      <nav className='p-5 bg-greybackground shadow md:flex md:items-center md:justify-between'>
+        <div className='flex justify-between items-center'>
+          <span className='text-2xl font-fira cursor-pointer text-white'>
+            <img src={logo} alt="logo" className="inline h-11 mr-2" />
+            Rahmadyan
+          </span>
 
-          <div className='md:static absolute bg-greybackground md:min-h-fit min-h-[60vh] left-0 top-[-100%] md:w-auto w-full flex items-center px-5 '>
-            <ul className='flex md:flex-row flex-col  md:items-center md:gap-[4vw] gap-8 font-fira text-[19px]'>
-              <li>
-                <a href="#" className="flex items-center">
-                  <span className="text-purple-600">#</span>
-                  <span className="text-white">home</span>
-                </a>
-              </li>
+          <span className='text-3xl cursor-pointer md:hidden block'>
+            <img
+              src={menulogo}
+              alt="menulogo"
+              className="inline h-7"
+              data-status="menu" // Using a data attribute
+              onClick={(e) => Menu(e)}
+            />
+          </span>
+        </div>
 
-              <li>
-                <a href="#" className="flex items-center">
-                  <span className="text-purple-600">#</span>
-                  <span className="text-white">works</span>
-                </a>
-              </li>
-              
-              <li>
-                <a href="#" className="flex items-center">
-                  <span className="text-purple-600">#</span>
-                  <span className="text-white">about-me</span>
-                </a>
-              </li>
-  
-              <li>
-                <a href="#" className="flex items-center">
-                  <span className="text-purple-600">#</span>
-                  <span className="text-white">contacts</span>
-                </a>
-              </li>
-            </ul>
-          
-          
-          </div>
-           <div className='flex items-end md:gap-6'>
-            <button className="flex items-center" >
-            <img src={menulogo} alt="logo" className='h-7 mr-2 cursor-pointer md:hidden' />
-            </button>
-        </div>
-         
-        </div>
+        <ul className='font-fira md:flex md:items-center text-[20px] z-[-1] md:z-auto
+          md:static absolute bg-greybackground w-full left-0 md:w-auto md:py-0 py-4 md:pl-0
+          pl-7 md:opacity-100 opacity-0 top-[-400px] transition-all ease-in duration-500'>
+          <li className='mx-4 my-5 md:my-0 '>
+            <a href="#" className="">
+              <span className="text-purple-600">#</span>
+              <span className="text-white hover:text-purple-600">home</span>
+            </a>
+          </li>
+
+          <li className='mx-4 my-5 md:my-0'>
+            <a href="#" className="flex items-center">
+              <span className="text-purple-600">#</span>
+              <span className="text-white hover:text-purple-600">works</span>
+            </a>
+          </li>
+
+          <li className='mx-4 my-5 md:my-0'>
+            <a href="#" className="flex items-center">
+              <span className="text-purple-600">#</span>
+              <span className="text-white hover:text-purple-600">about-me</span>
+            </a>
+          </li>
+
+          <li className='mx-4 my-5 md:my-0'>
+            <a href="#" className="flex items-center">
+              <span className="text-purple-600">#</span>
+              <span className="text-white hover:text-purple-600">contacts</span>
+            </a>
+          </li>
+        </ul>
       </nav>
     </div>
   );
